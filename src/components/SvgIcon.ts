5e3cb7082219eca447241e03b98d997acc77f362
Ferllings <contact@ferllings.me>
2026-03-20 14:09:22 +0100

    Minor build fix

--- src/components/SvgIcon.ts
+++ src/components/SvgIcon.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, svg, nothing } from 'lit';
+import { LitElement, html, css, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 const icons: Record<string, { viewBox: string; path: string }> = {
